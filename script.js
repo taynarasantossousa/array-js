a8907984401bc5aa5474a26453ff45382e8c6838
Taynara <taynarasantossousa09@gmail.com>
2025-04-08 21:17:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var carro = {
+    marca: "Ford",
+    modelo:"fusion",
+    ano: 2020,
+    portas:4,
+    cor: "Branco",
+    preco:126750,
+} 
+
+alert(`Marca: ${carro.marca} Modelo: ${carro.modelo} 
+Ano: ${carro.ano} 
+Portas: ${carro.portas} 
+Cor: ${carro.cor} Preco: ${carro.preco}`)
